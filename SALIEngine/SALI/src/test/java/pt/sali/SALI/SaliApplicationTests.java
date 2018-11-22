11bc41fcc90bd3ba3a1937ea659cd213978d6c76
Lushi <peepscallmelushi@gmail.com>
2018-11-22 19:28:15 +0000

    Introdução da engine

---
+++ SALIEngine/SALI/src/test/java/pt/sali/SALI/SaliApplicationTests.java
@@ -0,0 +1,16 @@
+package pt.sali.SALI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaliApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
