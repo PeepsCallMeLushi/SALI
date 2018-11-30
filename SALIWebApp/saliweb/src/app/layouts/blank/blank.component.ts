f792196590285abcf96b6690e5f48f7a78354d0b
Rusky <informatic5.0@gmail.com>
2018-11-30 01:51:07 +0000

    Removed shit

--- SALIWebApp/saliweb/src/app/layouts/blank/blank.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'blank-layout',
-  templateUrl: './blank.component.html',
-  styleUrls: []
-})
-export class BlankComponent {
-}
--- SALIWebApp/saliweb/src/app/sample-pages/timeline/timeline.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component} from '@angular/core';
-@Component({
-	templateUrl: 'timeline.component.html' 
-})
-export class TimelineComponent {
-	
-}
